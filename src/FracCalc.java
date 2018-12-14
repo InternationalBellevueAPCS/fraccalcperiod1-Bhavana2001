24d35a64839b427e520bd005b27a5667872fa96f
s-gottepub <s-gottepub@49S50013230.it.bsd405.org>
2018-12-14 11:34:12 -0800

    test

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -8,13 +8,15 @@ public class FracCalc {
     public static void main(String[] args) 
     {
     	Scanner console = new Scanner(System.in);
-    	
-    	System.out.println("enter numbers"); //removeable
+    	//allow the user to input equations
+    	//System.out.println("enter numbers");
     	String originalInput = console.nextLine();
     	
+    	//make a while loop the allows the user to quit when they type "quit"
     	while (! originalInput.equals("quit")) {
+    		//call the produceAnswer method
     		produceAnswer(originalInput);
-    		System.out.println("enter numbers"); //removeable
+    		System.out.println("enter numbers");
     		originalInput = console.nextLine();
     		
     	}
@@ -31,8 +33,10 @@ public class FracCalc {
      * @return the result of the fraction after it has been calculated.
      *      Example: return ==> "1_1/4"
      */
+    //create produceAnswer method
     public static String produceAnswer(String input)
     { 
+    	// the following are intializations of the whole, numerator and denominator for the first and second operand
     	String firstWhole= "0";
 		String firstNumerator= "0";
 		String firstDenominator= "0";
@@ -41,24 +45,21 @@ public class FracCalc {
 		String secondNumerator= "0";
 		String secondDenominator= "0";
 			
+		// use indexOf to find out where the spaces in the input are
     	int spaces = input.indexOf(" ");
     	
-    	// first operand
+    	// intialize firstOperand, with the given index
     	String firstOperand = input.substring(0, spaces);
-    	//System.out.println(firstOperand);
     	
-    	// operation
+    	// intialize operation,  with the given index
     	String operation = input.substring(spaces+1,spaces+2 );
-    	//System.out.println(operation);
     	
-    	//System.out.println(spaces); [this is just to check and it can be ignored]
+    	// intialize secondOperand,  with the given index
     	String secondOperand = input.substring((spaces+3), input.length());
+    	//print it for checkk point 1
     	System.out.println(secondOperand);
-    	
-    	//System.out.println(input);
-    	
-    	
-    	
+    		
+    	//use if loop to find out if there is a whole number in the first operand
     	if (firstOperand.indexOf("_")!= -1) {
     		firstWhole= firstOperand.substring(0,(firstOperand.indexOf("_")));
     		
@@ -116,28 +117,60 @@ public class FracCalc {
     	int newSecondWhole=Integer.parseInt(secondWhole);
     	int newSecondNumerator=Integer.parseInt(secondNumerator);
     	int newSecondDenominator=Integer.parseInt(secondDenominator);
-    	int newDenominator =0; 
     	
-    	if (newFirstDenominator==newSecondDenominator) {
-    		newDenominator = newSecondDenominator ;
-    		
-    		
-    	}else{
-    		newDenominator =newFirstDenominator*newSecondDenominator ;
-    	}
-    	
-    	newFirstNumerator= (newFirstNumerator*newSecondDenominator);
-		newSecondNumerator = (newSecondNumerator*newFirstDenominator);
-		//System.out.println("newDenominator = " + newDenominator);
-		//System.out.println("newFirstNumerator= " +newFirstNumerator);
-		//System.out.println("newSecondNumerator= " + newSecondNumerator);
+    	int newDenominator = 0; 
     	int newNumerator = 0;
-    	int newWhole = newFirstWhole + newSecondWhole;
-		if (operation.equals("+")) {
-			newNumerator = newFirstNumerator + newSecondNumerator;
+    	int newWhole=0;
+    	
+    	if (operation.equals("+") || operation.equals("-")) {
+    		
+    		if (newFirstDenominator==newSecondDenominator) {
+    			newDenominator = newSecondDenominator ;    		
+    		
+    		}else{
+    		newDenominator =newFirstDenominator*newSecondDenominator ;
+    		
+    		}
+    		
+    		newFirstNumerator= (newFirstNumerator*newSecondDenominator);    		
+    		newSecondNumerator = (newSecondNumerator*newFirstDenominator);    		
+    		newFirstNumerator += (newFirstWhole *newDenominator);    		
+			newSecondNumerator += (newSecondWhole *newDenominator);
 			
+    		if (operation.equals("+")) {
+    			newNumerator = newFirstNumerator + newSecondNumerator;
+    		}else if (operation.equals("-")) {
+    			newNumerator = newFirstNumerator - newSecondNumerator;
+    		}
+			
+		}else if (operation.equals("*")||operation.equals("/")) {
+			
+			newFirstNumerator= (newFirstWhole*newFirstDenominator) + newFirstNumerator;
+			newSecondNumerator= (newSecondWhole*newSecondDenominator) + newSecondNumerator;	
+			
+			if (operation.equals("*")) {
+				newNumerator = newFirstNumerator * newSecondNumerator;			
+				newDenominator = newFirstDenominator * newSecondDenominator;
+			}
+			
+			else if (operation.equals("/")) {			
+				newNumerator = newFirstNumerator * newSecondDenominator;
+				newDenominator = newFirstDenominator * newSecondNumerator;			
+			}
 		}
-		System.out.println(newWhole + "_" + newNumerator+ "/" + newDenominator);
+    	if (newNumerator>=newDenominator) {
+			newWhole = newNumerator/newDenominator;
+			newNumerator = newNumerator%newDenominator;
+		}
+    	if (newWhole != 0) {
+    		System.out.print(newWhole + "_");
+    		greatestCommonDivisor(newNumerator,newDenominator);
+    	}
+    	else if (newWhole == 0) {
+    		greatestCommonDivisor(newNumerator,newDenominator);
+    	
+    	}
+		//System.out.println(newWhole + "_" + newNumerator+ "/" + newDenominator);
         // TODO: Implement this function to produce the solution to the input
         // Checkpoint 1: Return the second operand.  Example "4/5 * 1_2/4" returns "1_2/4".
         // Checkpoint 2: Return the second operand as a string representing each part.
@@ -174,7 +207,9 @@ public class FracCalc {
             min = max % min;
             max = tmp;
         }
-        System.out.print(max);
+        int finalNumerator = a/max;
+        int finalDenominator = b/max;
+        System.out.println(finalNumerator+ "/" + finalDenominator);
         return max;
         
     }
